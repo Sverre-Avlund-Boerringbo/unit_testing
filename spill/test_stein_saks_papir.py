1a1d2f3b5be027ab65d159bcf2c0dc47fa59a68d
Sverre-Avlund-Boerringbo <svboa002@osloskolen.no>
2024-05-08 10:32:25 +0200

    Update test_stein_saks_papir.py

--- spill/test_stein_saks_papir.py
+++ spill/test_stein_saks_papir.py
@@ -5,12 +5,17 @@ STEIN = 0
 SAKS = 1
 PAPIR = 2
 
+# Enkel test:
+def test_stein_saks_papir():
+    assert klasser.stein_saks_papir(SAKS, STEIN) == STEIN
 
+
+# Test med parametere:
 @pytest.mark.parametrize("type1,type2,resultat", [
     (STEIN, SAKS, STEIN),
     (PAPIR, STEIN, PAPIR),
     (PAPIR, SAKS, SAKS),
     (STEIN, STEIN, STEIN),
 ])
-def test_stein_saks_papir(type1, type2, resultat):
+def test_stein_saks_papir_parameter(type1, type2, resultat):
     assert klasser.stein_saks_papir(type1, type2) == resultat
